2cbbf63cbf19b668e5baedb849fecbae5faf8e48
no5td <no5@no5td.com>
2017-07-06 17:12:27 +0900

    second commit

--- jellyfish.rb
+++ jellyfish.rb
@@ -4,28 +4,32 @@ jellyfish = ["グー","チョキ","パー"]
 for i in 0...5
   judg = 0
   while judg <= 0
-    puts "#{i}回戦目 (グー:1 チョキ:2 パー:3を入力)"
+    puts "#{i + 1}回戦目 (グー:1 チョキ:2 パー:3を入力)"
     manplayer = gets.to_i
     while (manplayer <= 0) || (manplayer > 3)
       puts "1～3までの数字を入力してください。"
       manplayer = gets.to_i
     end
-    p manplayer
     puts "You:" + jellyfish[manplayer - 1]
-    random = rand(0..2) + 1
-    complayer = random
+    if i <= 3 && yourScore == 2
+      puts "インチキモード発動"
+      complayer = (manplayer - 1 + 3) % 3;
+    else
+      random = rand(0..2) + 1
+      complayer = random
+    end
     puts "COM:" + jellyfish[complayer - 1]
     judg = (manplayer - complayer + 3) % 3
     if judg == 1
-      puts "Com 勝ち"
-      yourScore += 1
-    elsif judg == 2
-      puts "You 勝ち"
+      print "Com 勝ち"
       comScore += 1
+    elsif judg == 2
+      print "You 勝ち"
+      yourScore += 1
     else
-      print "あいこなので再戦\n"
+      print "あいこなので再戦"
     end
-    puts " You:" + yourScore + "勝 Com:" + comScore + "勝"
+    print " You:" , yourScore , "勝 Com:" , comScore , "勝\n\n"
   end
 end
 puts "-------------------------------------"
